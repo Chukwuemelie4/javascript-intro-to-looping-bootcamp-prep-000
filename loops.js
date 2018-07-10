95abce2c4560e9deefe9855fcc0b5ad24cbd6fd6
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-07-10 18:11:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 array= []
 for (var i = 0; i < 25; i++) {
+<<<<<<< HEAD
   array.push(`I am ${i} strange loop${i===0 ? '' : 's'}.`)
+=======
+  array.push(`I am ${i} strange loop.`)
+>>>>>>> d3fc8ef178f3ecadb6d328aef75afcfd9b3c8a7c
 }
 console.log(array);
